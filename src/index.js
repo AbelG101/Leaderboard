00fd07d049ac55168ef8a12040aab90b9f01a64a
AbelG101 <abelgebeyehu101@gmail.com>
2022-08-24 19:05:12 +0300

    add event listeners for the buttons

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 import './styles.css';
+import addScore from './addScore';
+import getScores from './getScore';
+
+const refreshBtn = document.querySelector('.refresh-btn');
+const submitBtn = document.querySelector('.submit-btn');
+
+refreshBtn.addEventListener('click', () => {
+  getScores();
+});
+
+submitBtn.addEventListener('click', () => {
+  addScore();
+});
