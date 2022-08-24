473f60c37bfe2d2b0a3b2e16278c96ffe82a64f9
AbelG101 <abelgebeyehu101@gmail.com>
2022-08-24 19:11:50 +0300

    add function to get score from the API

---
+++ src/getScore.js
@@ -0,0 +1,15 @@
+import renderScoreOnPage from './renderPage';
+
+const getScores = async () => {
+  const gameApiUrl = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/';
+  const id = 'Zl4d7IVkemOTTVg2fUab';
+  const scoresUrl = `${gameApiUrl}${id}/scores/`;
+  const response = await fetch(scoresUrl);
+  const players = (await response.json()).result;
+  document.querySelector('.table').innerHTML = '';
+  players.forEach((player) => {
+    renderScoreOnPage(player);
+  });
+};
+
+export default getScores;
